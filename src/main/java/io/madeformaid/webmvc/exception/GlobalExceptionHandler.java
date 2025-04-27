30997cae95662196d1f3b300ef5580b4044f2435
ish <tmdghks0615@gmail.com>
2025-04-28 00:55:46 +0900

    기본 예외처리 추가

--- src/main/java/io/madeformaid/webmvc/exception/GlobalExceptionHandler.java
+++ src/main/java/io/madeformaid/webmvc/exception/GlobalExceptionHandler.java
@@ -39,6 +39,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), "요청 파라미터 타입이 올바르지 않습니다."));
+                .body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), "올바르지 않은 요청 입니다. 다시 시도해주세요."));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error("Internal server error", ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "시스템 오류가 발생했습니다. 다시 시도하거나 관리자에게 문의해주세요."));
     }
 }
